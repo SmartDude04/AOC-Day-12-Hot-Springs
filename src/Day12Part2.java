7bc7ea4204bd8fcc57506d3e22edfa66b8589c33
Aidan O'Connor <aidan.j.oconnor@outlook.com>
2024-03-28 10:05:14 -0400

    May start part 2

---
+++ src/Day12Part2.java
@@ -0,0 +1,10 @@
+import java.util.HashMap;
+
+public class Day12Part2
+{
+    public static HashMap<String, Integer> cache = new HashMap<>();
+    public static void main(String[] args)
+    {
+
+    }
+}
